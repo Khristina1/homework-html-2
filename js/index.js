9dd5c79ea70878e23b1855c8eace101daf086cc3
Khristina1 <sokeyush@gmail.com>
2019-09-11 12:57:58 +0000

    New JS

---
+++ js/index.js
@@ -0,0 +1,12 @@
+//clock
+const clockElement = document.querySelector('.clock');
+setInterval( () => clockElement.innerText = (new Date ()).toLocaleTimeString(), 1000);
+
+document.querySelectorAll('.product-buttons .button').forEach(button => button.addEventListener('click', buttonPressed));
+function buttonPressed(ev){
+    alert('Добавлено в корзину');
+}
+document.querySelectorAll('.catagories .catagory').forEach(catagory => catagory.addEventListener('click', catagoryPressed));
+function catagoryPressed(ev){
+    alert('Ви переходите до іншої категорії');
+}
