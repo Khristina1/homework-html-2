2a161c7743f90745f14f181c48f971420b872eb7
Khristina1 <sokeyush@gmail.com>
2019-09-13 15:08:50 +0000

    New

--- js/index.js
+++ js/index.js
@@ -2,16 +2,25 @@
 const clockElement = document.querySelector('.clock');
 setInterval( () => clockElement.innerText = (new Date ()).toLocaleTimeString(), 1000);
 
+$('.main').click(function(){
+    $('.main').toggleClass('animated flipInX');
+});
+$('.catalog').click(function(){
+    $('.catalog').toggleClass('animated flipInX');
+});
+$('.contacts').click(function(){
+    $('.contacts').toggleClass('animated flipInX');
+});
+
 document.querySelectorAll('.product-buttons .buttonBuy').forEach(buttonBuy => buttonBuy.addEventListener('click', buttonBuyPressed));
 function buttonBuyPressed(ev){
     alert('Добавлено в корзину');
-}
+};
 document.querySelectorAll('.catagories .catagory').forEach(catagory => catagory.addEventListener('click', catagoryPressed));
 function catagoryPressed(ev){
     alert('Ви переходите до іншої категорії');
-}
+};
 document.querySelectorAll('#contacts .contact').forEach(contact => contact.addEventListener('click', contactPressed));
  function contactPressed(ev){
      alert ('Зателефонуйте нам)');
-     }
-
+     };
